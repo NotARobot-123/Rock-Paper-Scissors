2446958b83c59e0d27a8ddf8e4c4587042d31a1a
NotARobot-123 <grayden.farrer@papamoacollege.school.nz>
2026-03-03 18:26:44 +1300

    Add files via upload

---
+++ string checker v.3.py
@@ -0,0 +1,43 @@
+#Name: String Checker
+#By: Grayden Farrer
+#This code will check that the outputs from the string match the expected output.
+
+
+#Check that users have entered a valid option based on list
+def string_checker(question, valid_ans=None):
+
+    if valid_ans is None:
+        valid_ans = ['yes','no']
+
+    error = f"Please enter a valid option from the following list: {valid_ans}"
+
+    while True:
+
+        # get user response and make sure its lowercase
+        user_response = input(question).lower()
+
+        for item in valid_ans:
+            #check if the user response is a word on the list
+            if item == user_response:
+                return item
+
+            #checks if the user response is the same as
+            #the first letter of an item in the list
+            elif user_response == item[0]:
+                return item
+
+        #print error if user does not enter valid answer
+        print(error)
+        print()
+
+
+# Main routine goes here
+
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+want_instructions = string_checker("Do you want to see the instructions?",)
+
+print("you chose: ", want_instructions)
+
+user_choice = string_checker("choose rock/paper/scissors: ", rps_list)
+print("you chose: ", user_choice)
